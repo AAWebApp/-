9ac87bc843b94e08b6d2763b95499a14cebd8e64
AAWebApp <99390557+AAWebApp@users.noreply.github.com>
2022-02-11 11:49:17 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -3,13 +3,14 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import jieba
 
 """
-# Welcome to Streamlit!
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+# 简政
+欢迎进入简政元空间！ :heart:
+以下可以忽略[documentation](https://docs.streamlit.io) ，[community
 forums](https://discuss.streamlit.io).
-In the meantime, below is an example of what you can do with just a few lines of code:
+
 """
 
 
