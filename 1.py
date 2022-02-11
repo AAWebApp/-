588ce7a5552bb38de8cefb8a2140d322e576a1b8
AAWebApp <99390557+AAWebApp@users.noreply.github.com>
2022-02-11 15:30:09 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -11,6 +11,16 @@ import jieba
 这两个东西可以忽略[不要点我](https://docs.streamlit.io) ，[不要点我](https://discuss.streamlit.io).
 
 """
+xm = st.text_input('请在这里输入你的姓名 :heart:')
+if st.button('确定'):
+    if xm=="刘杨滢":
+        st.write("知道吗，一个人几乎可以在任何他怀有无限热忱的事情上成功。")
+    if xm=='朱冠源':
+        st.write("如果不想做点事情，就不要想到达这个世界上的任何地方。")
+    if xm=='卓靖东':
+        st.write("人生舞台的大幕随时都可能拉开，关键是你愿意表演，还是选择躲避。")
+    if xm=='金业成':
+        st.write("失败是什么？没有什么，只是更走近成功一步；成功是什么？就是走过了所有通向失败的路，只剩下一条路，那就是成功的路。")
 
 t = st.text_input('分词器', '请输入一段文字')
 if st.button('确定'):
@@ -19,6 +29,9 @@ if st.button('确定'):
     st.write("输出结果是")
     for i in j:
         st.write(i)
+"""
+# 下面是对几篇政策的词性词频统计
+"""
     
 from jieba import posseg
 import jieba
@@ -45,6 +58,9 @@ st.write(len(n))
 for i in range(5):
     q,w=n[i]
     st.write("{0: <20}{1: >20}".format(q,w))
+"""
+# 下面是无关的其他代码
+"""
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
