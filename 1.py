94dba2be14abb94a8b9aad68ba7effa2161aa707
AAWebApp <99390557+AAWebApp@users.noreply.github.com>
2022-02-11 11:52:02 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -12,7 +12,20 @@ import jieba
 forums](https://discuss.streamlit.io).
 
 """
-
+st.write("""# 我的第一个网站""")
+t = st.text_input('分词器', '输入一段文字')
+if st.button('确定'):
+    import jieba
+    j=jieba.cut(t)
+    st.write("输出结果是")
+    for i in j:
+        st.write(i)
+    
+else:
+    st.write('我哈哈大笑')
+st.write("词典输出功能的试用")
+cidian={"功能1":"分词器","功能2":"哈哈大笑"}
+st.write(cidian)
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
