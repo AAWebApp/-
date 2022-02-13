c4430cb95e52169b22db23881e662f09444c50e1
AAWebApp <99390557+AAWebApp@users.noreply.github.com>
2022-02-13 18:51:37 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -3,59 +3,14 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-import jieba
 
 """
-# 简政
-欢迎进入简政元空间！ :heart:
-这两个东西可以忽略[不要点我](https://docs.streamlit.io) ，[不要点我](https://discuss.streamlit.io).
-
+# Welcome to Streamlit!
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
+In the meantime, below is an example of what you can do with just a few lines of code:
 """
-xm = st.text_input('请在这里输入你的姓名 ')
-if st.button('确定'):
-    if xm=="刘杨滢":
-        st.write("知道吗，一个人几乎可以在任何他怀有无限热忱的事情上成功。")
-    if xm=='朱冠源':
-        st.write("如果不想做点事情，就不要想到达这个世界上的任何地方。")
-    if xm=='卓靖东':
-        st.write("人生舞台的大幕随时都可能拉开，关键是你愿意表演，还是选择躲避。")
-    if xm=='金业成':
-        st.write("失败是什么？没有什么，只是更走近成功一步；成功是什么？就是走过了所有通向失败的路，只剩下一条路，那就是成功的路。")
-
-t = st.text_input('分词器', '请输入一段文字')
-#if st.button('确定'):
-    
-    #j=jieba.cut(t)
-    #st.write("输出结果是")
-    #for i in j:
-        #st.write(i)
-
-    
-from jieba import posseg
-import jieba
-#h=input("粘贴输入长文本：")
-h=open("大实验1.txt","r",encoding='utf-8').read()
-c=posseg.cut(h)
-d={}
-e={}
-for i1,i2 in c:
-    i3=i1+i2#将元组合并为字符串
-    if len(i1)==1:
-        continue
-    if i2!="v"and"vn":
-        continue
-        
-    a=d.get(i3,0)
-    d[i3]=a+1
-#print(d)
-n=list(d.items())#将词典中的键值对转换成元组构成的列表
-#print(n)
-n.sort(key=lambda x:x[1],reverse=True)#降序排列
-#print(n)
-st.write(len(n))
-for i in range(5):
-    q,w=n[i]
-    st.write("{0: <20}{1: >20}".format(q,w))
 
 
 with st.echo(code_location='below'):
@@ -78,3 +33,14 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
